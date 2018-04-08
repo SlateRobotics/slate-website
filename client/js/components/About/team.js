a160cd62a978de86b12b8900cb9bbf943f439e94
Zach Allen <zallen@pension-consultants.com>
2018-04-07 23:06:40 -0500

    typo

--- client/js/components/About/team.js
+++ client/js/components/About/team.js
@@ -4,7 +4,7 @@ module.exports = [{
   title: "CEO and CTO",
   type: "team",
   img: "/img/zach",
-  description: "Zach founded Slate Robotics in August 2018 with the mission to make general purpose robots more affordable. He continues to oversee the company's product strategy -- including the design, engineering and manufacturing of these robots. Zach has been fascinated by personal robots for years. He designed and built the ground-breaking TR1 in his garage over the course of a year after trying to find an affordable robot development platform. Zach's background is in Software Engineering. He worked as a developer for 3 years after graduating from Drury University with a BA in Spanish.",
+  description: "Zach founded Slate Robotics in August 2017 with the mission to make general purpose robots more affordable. He continues to oversee the company's product strategy -- including the design, engineering and manufacturing of these robots. Zach has been fascinated by personal robots for years. He designed and built the ground-breaking TR1 in his garage over the course of a year after trying to find an affordable robot development platform. Zach's background is in Software Engineering. He worked as a developer for 3 years after graduating from Drury University with a BA in Spanish.",
   linkedin: "https://www.linkedin.com/in/zachary-allen-674ab8a9/",
   twitter: "https://twitter.com/ZachAllen417",
   facebook: "https://www.facebook.com/zach.allen.566",
