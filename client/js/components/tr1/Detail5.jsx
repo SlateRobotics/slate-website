0449033782888f6a3e8f9f2dae60373de17ecc59
Zach Allen <zallen@pension-consultants.com>
2018-07-27 14:19:05 -0500

    changed warranty length

--- client/js/components/tr1/Detail5.jsx
+++ client/js/components/tr1/Detail5.jsx
@@ -13,9 +13,9 @@ var Component = React.createClass({
               <div className="row">
                 <div className="col-md-3 col-sm-6 col-xs-12">
                   <img src="/img/icon-warranty" style={{height:"75px",width:"75px"}} />
-                  <h3>90 Day Warranty</h3>
+                  <h3>1 Year Warranty</h3>
                   <p>
-                    The TR1 is backed by a 90 day warranty to give you peace of
+                    The TR1 is backed by a 1 year warranty to give you peace of
                     mind about your purchase.
                   </p>
                   <Link to="/sales-policies" style={{color:"#fff"}}>
