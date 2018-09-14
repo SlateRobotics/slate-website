8164d3e5e792f9e3a8fc11831efdcfcfaf139c26
Zach417 <zach@slaterobots.com>
2018-09-14 14:34:07 -0500

    added production manager to careers

--- client/js/components/Careers/application.js
+++ client/js/components/Careers/application.js
@@ -1,7 +1,7 @@
 module.exports = {
   questions: [{
-    type: "textarea",
     name: "List any relevant experience that you think will be most beneficial to your performance in this position.",
+    type: "textarea",
   }, {
     name: "What is the most impressive thing that you have accomplished in your life?",
     type: "textarea",
@@ -13,9 +13,6 @@ module.exports = {
     type: "textarea",
   }, {
     name: "Some believe that Artificial Intelligence is an existential threat to humanity. What are their arguments? What are some fundamental flaws, if any, in their thinking?",
-    type: "textarea",
-  }, {
-    name: "Jeff is the project lead for a new feature on the TR1 that is due tomorrow. John and Sarah, Jeff's engineers, are under a heavy work load and are late to getting the completed design to him; it is looking like they are going to be late on completing the project. Who is ultimately to blame here? What, if anything, could the guilty party have done to prevent this? What are the next steps?",
     type: "textarea",
   }]
 }
